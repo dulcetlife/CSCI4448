5e261650663227d438cad8a8075307ce3a6b505b
s h <s h>
2016-04-12 15:54:38 -0600

    Added source code base

---
+++ OpenSourceWebsite/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name openSourceWebsiteApp
+ * @description
+ * # openSourceWebsiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('openSourceWebsiteApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ OpenSourceWebsite/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name openSourceWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the openSourceWebsiteApp
+ */
+angular.module('openSourceWebsiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
